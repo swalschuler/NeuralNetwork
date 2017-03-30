d1a59491039ae94f849f59995d36bc247dc1503e
Schuyler Alschuler <Schuyler Alschuler>
2017-03-30 13:26:45 -0700

    Adds ability to add two one bit numbers

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ from NeuralNet import NeuralNetwork, NeuronLayer
 
 random.seed(1)
 
-layer1 = NeuronLayer(4, 3)
-layer2 = NeuronLayer(1, 4)
+layer1 = NeuronLayer(3, 2)
+layer2 = NeuronLayer(2, 3)
 
 neuralNet = NeuralNetwork(layer1, layer2)
 
-neuralNet.printWeights()
+#neuralNet.printWeights()
 
-inputs = array([[0, 0, 1], [0, 1, 1], [1, 0, 1], [0, 1, 0], [1, 0, 0], [1, 1, 1], [0, 0, 0]])
-outputs = array([[0, 1, 1, 1, 1, 0, 0]]).T
-
+inputs = array([[0, 0], [0, 1], [1, 1]])
+outputs = array([[0,0], [0, 1], [1, 0]])
+print(outputs)
 neuralNet.train(inputs, outputs, 60000)
 
-neuralNet.printWeights()
+#neuralNet.printWeights()
 
-hiddenOutput, output = neuralNet.think(array([1, 1, 0]))
+hiddenOutput, output = neuralNet.think(array([1, 0]))
 print(output)
